e6703c539f0399d38e4354633bd6c87cb2619b9e
Ishwari-4 <ishwarinaik2003@gmail.com>
2025-09-14 15:15:47 +0530

    Update models.py

--- FlaskWebProject/models.py
+++ FlaskWebProject/models.py
@@ -2,16 +2,21 @@ from datetime import datetime
 from FlaskWebProject import app, db, login
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
-from azure.storage.blob import BlockBlobService
+from azure.storage.blob import BlobServiceClient
 import string, random
 from werkzeug.utils import secure_filename
 from flask import flash
+import os
 
-blob_container = app.config['BLOB_CONTAINER']
-blob_service = BlockBlobService(account_name=app.config['BLOB_ACCOUNT'], account_key=app.config['BLOB_STORAGE_KEY'])
+# Azure Blob Storage setup
+blob_container = os.environ['BLOB_CONTAINER']
+blob_service_client = BlobServiceClient.from_connection_string(os.environ['BLOB_STORAGE_KEY'])
+container_client = blob_service_client.get_container_client(blob_container)
 
+# Helper function to generate random filenames
 def id_generator(size=32, chars=string.ascii_uppercase + string.digits):
     return ''.join(random.choice(chars) for _ in range(size))
+
 
 class User(UserMixin, db.Model):
     __tablename__ = 'users'
@@ -20,7 +25,7 @@ class User(UserMixin, db.Model):
     password_hash = db.Column(db.String(128))
 
     def __repr__(self):
-        return '<User {}>'.format(self.username)
+        return f'<User {self.username}>'
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
@@ -28,9 +33,11 @@ class User(UserMixin, db.Model):
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
 
+
 @login.user_loader
 def load_user(id):
     return User.query.get(int(id))
+
 
 class Post(db.Model):
     __tablename__ = 'posts'
@@ -43,7 +50,7 @@ class Post(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
 
     def __repr__(self):
-        return '<Post {}>'.format(self.body)
+        return f'<Post {self.body}>'
 
     def save_changes(self, form, file, userId, new=False):
         self.title = form.title.data
@@ -52,17 +59,24 @@ class Post(db.Model):
         self.user_id = userId
 
         if file:
-            filename = secure_filename(file.filename);
-            fileextension = filename.rsplit('.',1)[1];
-            Randomfilename = id_generator();
-            filename = Randomfilename + '.' + fileextension;
+            filename = secure_filename(file.filename)
+            file_extension = filename.rsplit('.', 1)[1]
+            random_filename = id_generator()
+            filename = f"{random_filename}.{file_extension}"
+
             try:
-                blob_service.create_blob_from_stream(blob_container, filename, file)
-                if(self.image_path):
-                    blob_service.delete_blob(blob_container, self.image_path)
-            except Exception:
-                flash(Exception)
-            self.image_path =  filename
+                # Upload new blob
+                container_client.upload_blob(name=filename, data=file, overwrite=True)
+
+                # Delete old blob if exists
+                if self.image_path:
+                    container_client.delete_blob(self.image_path)
+
+            except Exception as e:
+                flash(f"Blob error: {e}")
+
+            self.image_path = filename
+
         if new:
             db.session.add(self)
         db.session.commit()
